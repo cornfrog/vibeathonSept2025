282901c0e2570ac124389d458d785cf67fe57d54
Corn <cornfrog@gmail.com>
2025-09-19 20:02:25 -0400

    added tools list

---
+++ src/tools.jsx
@@ -0,0 +1,19 @@
+const toolsList = [
+    "Hammer",
+    "Screwdriver (flathead)",
+    "Screwdriver (Phillips)",
+    "Tape measure",
+    "Adjustable wrench",
+    "Pliers",
+    "Utility knife",
+    "Level",
+    "Power drill",
+    "Hex key (Allen wrench)",
+    "Handsaw",
+    "Flashlight",
+    "Stud finder",
+    "Step stool",
+    "Duct tape"
+]
+
+export default toolsList;
